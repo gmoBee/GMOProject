a247f7a5371ab2b4e675cacb344b8f65539ace30
gmoBee <mr.ggn@list.ru>
2020-10-18 10:00:09 +1300

    Little changes, added bullets

---
+++ Assets/bulletScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class bulletScript : MonoBehaviour
+{
+
+    public Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid.velocity = transform.forward * sho
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
